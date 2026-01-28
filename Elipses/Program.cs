364dba4bb70d7149343cb47ee15eb2e267e5202f
Jason <jason_morgan84@yahoo.co.uk>
2026-01-28 15:50:24 +0000

    Fixed sobel cernel bug and added non maximum suppression

--- Elipses/Program.cs
+++ Elipses/Program.cs
@@ -17,6 +17,10 @@ namespace Ellipses
 
         static void Main(string[] args)
         {
+
+
+            /******************PRIORITY - Compare output so far to other versions online*********************/
+
             //Things that are done:
             ///////////Load test image
             ///////////Dapi channel into array
@@ -24,6 +28,8 @@ namespace Ellipses
             ///////////Guassian blur
 
             //Things to do
+            ///////////Double thresholding
+            ///////////Hysteresis
             ///////////Adapt to multi slice image
             ///////////Elevation Map
             ///////////Watershedding
@@ -36,13 +42,13 @@ namespace Ellipses
             int channels = 4;
             int slices;
 
+            //Image InputImage = Image.FromFile(@"rectangle.tif");
             Image InputImage = Image.FromFile(@"SingleSliceTest.tif");
             int pageCount = InputImage.GetFrameCount(System.Drawing.Imaging.FrameDimension.Page);
 
             slices = pageCount / channels;
             int ImageHeight=InputImage.Height;
             int ImageWidth=InputImage.Width;
-            //add variables for image height/width
 
             InputImage.SelectActiveFrame(System.Drawing.Imaging.FrameDimension.Page,SegmentChannel-1);
 
@@ -64,67 +70,81 @@ namespace Ellipses
 
             int[,] GaussianBlurred = new int[ImageHeight, ImageWidth];
             GaussianBlurred = Program.GaussianBlur(ImageArray, ImageHeight, ImageWidth, 3);
-
-            
+            Console.WriteLine("Gaussian blur complete");
             int[,] EdgeDetected = new int[ImageHeight, ImageWidth];
-            int[,] Gradients = new int[ImageHeight, ImageWidth];
-            Program.SobelEdge(GaussianBlurred,out EdgeDetected,out Gradients);
-
-            
+            double[,] Gradients = new double[ImageHeight, ImageWidth];
+            int[,] NMS = new int[ImageHeight, ImageWidth];
+            //note: Gaussian blur skipped for testing
+            Program.SobelEdge(GaussianBlurred, ImageHeight, ImageWidth, out EdgeDetected,out Gradients);
+            Console.WriteLine("Edge detection complete");
+            NMS = Program.NonMaximumSuppression(EdgeDetected, Gradients, ImageHeight, ImageWidth);
+            Console.WriteLine("Non-maximum suppression complete");
+                        
             Bitmap OutputBitmap = new Bitmap(ImageWidth, ImageHeight , System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
             
-            for (int y = 0; y < ImageHeight - 1; y++)
+            for (int y = 0; y < ImageHeight; y++)
             {
-                for (int x = 0; x < ImageWidth - 1; x++)
+                for (int x = 0; x < ImageWidth; x++)
                 {
-                    OutputBitmap.SetPixel(x, y, Color.FromArgb(255, Gradients[y, x]*64, Gradients[y, x]*64, Gradients[y, x]*64));
+                    OutputBitmap.SetPixel(x, y, Color.FromArgb(255, EdgeDetected[y, x], EdgeDetected[y, x], EdgeDetected[y, x]));
                 }
             }
             OutputBitmap.Save("Edgy.tif");
+
+            Bitmap OutputBitmap2 = new Bitmap(ImageWidth, ImageHeight, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+            for (int y = 0; y < ImageHeight; y++)
+            {
+                for (int x = 0; x < ImageWidth; x++)
+                {
+                    OutputBitmap2.SetPixel(x, y, Color.FromArgb(255, NMS[y, x], NMS[y, x], NMS[y, x]));
+                }
+            }
+            OutputBitmap2.Save("thinned.tif");
             
         }
 
-        static void SobelEdge(int[,] SourceArray, out int[,] GradientIntensity, out int[,] EdgeDirection)
+        static void SobelEdge(int[,] SourceArray, int height, int width, out int[,] GradientIntensity, out double[,] EdgeDirection)
         {
             //returns array using sobel edge detection and intensity gradient for further steps of Canny edge detection
-            int height = SourceArray.GetLength(0);
-            int width = SourceArray.GetLength(1);
-
             double sx, sy;
             double[,] WorkingIntensity = new double[height, width];
             GradientIntensity = new int[height, width];
             double MaxIntensity = 0;
 
             double G = 0;
-            EdgeDirection = new int[height, width];
+            EdgeDirection = new double[height, width];
 
 
-            for (int y = 1; y< height - 1;y++)
+            for (int y = 1; y < height - 1; y++)
             {
-                for(int x = 1; x<width-1;x++)
+                for (int x = 1; x < width - 1; x++)
                 {
                     //for each pixel (excluding top row, bottom row, left column and right column)
                     //convolve with both sobel operators
                     //square root sum of squares to get new value
-                    sx = (1 * SourceArray[y - 1, x - 1] + 2 * SourceArray[y - 1, x] + 1 * SourceArray[y - 1, x + 1] - 1 * SourceArray[y + 1, x - 1] - 2 * SourceArray[y + 1, x] - 1 * SourceArray[y + 1, x + 1]);
-                    sy = (1 * SourceArray[x - 1, y - 1] + 2 * SourceArray[x - 1, y] + 1 * SourceArray[x - 1, y + 1] - 1 * SourceArray[x + 1, y - 1] - 2 * SourceArray[x + 1, y] - 1 * SourceArray[x + 1, y + 1]);
+                    sx = (-1 * SourceArray[y - 1, x - 1] + 1 * SourceArray[y - 1, x + 1]
+                        - 2 * SourceArray[y, x - 1] + 2 * SourceArray[y, x+1]
+                        - 1 * SourceArray[y + 1, x - 1] + 1 * SourceArray[y + 1, x + 1]);
+                    sy = (-1 * SourceArray[y - 1, x - 1] -2 * SourceArray[y - 1, x] -1 * SourceArray[y - 1, x + 1] 
+                        + 1 * SourceArray[y + 1, x - 1] + 2 * SourceArray[y + 1, x] + 1 * SourceArray[y + 1, x + 1]);
                     WorkingIntensity[y,x] = Math.Sqrt(sx * sx + sy * sy);
+
+                    //get max intensity for normalisation
                     if (WorkingIntensity[y, x] > MaxIntensity) MaxIntensity = WorkingIntensity[y,x];
 
+                    //calculate gradient angle in degrees
                     G = Math.Atan2(sy , sx)*(180/Math.PI);
-
-                    if ((G > 0 - 22.5 && G <= 0 + 22.5) || (G > -180 - 22.5 && G <= -180 + 22.5) || (G > 180 - 22.5 && G <= 180 + 22.5)) { G = 0; } //Round to G = 0 for horizontal
-                    else if ((G > 45 - 22.5 && G <= 45 + 22.5) || (G > -135 - 22.5 && G <= -135 + 22.5)) { G = 1; } // Round to G = 1 to diagonal increasing as x increases
-                    else if ((G > 90 - 22.5 && G <= 90 + 22.5) || G > -90 - 22.5 && G <= -90 + 22.5) { G = 2; } //Round to G = 2 for vertical
-                    else if ((G > 135 - 22.5 && G <= 135 + 22.5) || (G > -45 - 22.5 && G <= -45 + 22.5)) { G = 3; } // Round to G = 3 for diagonal decreasing as x increases
-
-                    if (G!=0 & G!= 1 & G!= 2 & G!=3) Console.WriteLine(G);
-                    EdgeDirection[y, x] = (int)G;
+                    EdgeDirection[y, x] = G;
+                  
+                    //Console.WriteLine("Y: {0}; X: {1}; Value: {2}; sx: {3}; sy: {4}; Int: {5}; G1: {6}; G2: {7}", y, x, SourceArray[y,x], sx, sy, Math.Sqrt(sx * sx + sy * sy), Math.Atan2(sy, sx) * (180 / Math.PI), G);
                 }
             }
-            for (int y = 1; y < height - 1; y++)
+            
+            //normalise gradient intensities
+            for (int y = 0; y < height; y++)
             {
-                for (int x = 1; x < width - 1; x++)
+                for (int x = 0; x < width; x++)
                 {
                     WorkingIntensity[y, x] = (WorkingIntensity[y, x] / MaxIntensity) * 255;
                     GradientIntensity[y, x] = (int)WorkingIntensity[y, x];
@@ -150,7 +170,6 @@ namespace Ellipses
             {
                 Kernel[i] /= KernelSum;
             }
-
             //Apply kernel to image in two passes
             for (int y = 1; y < Height - 1; y++)
             {
@@ -169,7 +188,6 @@ namespace Ellipses
                     FirstPass[y, x] = (int)XNewValue;
                 }
             }
-
             for (int y = 1; y < Height - 1; y++)
             {
                 for (int x = 1; x < Width - 1; x++)
@@ -182,19 +200,64 @@ namespace Ellipses
                         else if (YPosition >= Height) { YPosition = 2 * Height - YPosition - 1; }
 
                         YNewValue += FirstPass[YPosition, x] * Kernel[i];
-
-
                     }
                     OutputArray[y, x] = (int)YNewValue;
                 }                
             }
-            //consider neccessity of normalisation
             return OutputArray;
         }
 
-        static int[,] NonMaximumSuppression(int[,] SourceArray)
+        static int[,] NonMaximumSuppression(int[,] SourceArray, double[,] EdgeAngle, int Height, int Width)
         {
-            int[,] OutputArray = new int[SourceArray.GetLength(0), SourceArray.GetLength(1)];
+            int[,] OutputArray = new int[Height, Width];
+            for (int y = 1; y < Height - 1; y++)
+            {
+                for (int x = 1; x < Width - 1; x++)
+                {
+                    double G = EdgeAngle[y,x];
+                    //Round angles to 0, 45, 90 or 135
+                    //Console.Write("\n");
+                   // Console.Write("y: {0}, x: {1}, G: {2}, Value: {3}", y, x, G, SourceArray[y,x]);
+                    if ((G > 0 - 22.5 && G <= 0 + 22.5) || (G > -180 - 22.5 && G <= -180 + 22.5) || (G > 180 - 22.5 && G <= 180 + 22.5)) 
+                    {
+                        //for angles around 0 (horizontal), check east and west cells to see if current cell is the maximum (or and greater than equals to catch
+                        //if cells are identical
+                        if ((SourceArray[y, x] > SourceArray[y, x - 1] && SourceArray[y, x] >= SourceArray[y, x + 1]) ||
+                            (SourceArray[y, x] >= SourceArray[y, x - 1] && SourceArray[y, x] > SourceArray[y, x + 1]))
+                        {
+                            OutputArray[y, x] = SourceArray[y, x];
+                        }
+                    } 
+                    else if ((G > 45 - 22.5 && G <= 45 + 22.5) || (G > -135 - 22.5 && G <= -135 + 22.5))
+                    {
+                        //for angles around 45 degrees (y decreasing as x increases) check north-west and south east
+                        if ((SourceArray[y, x] > SourceArray[y - 1, x - 1] && SourceArray[y, x] >= SourceArray[y + 1, x + 1]) ||
+                            (SourceArray[y, x] >= SourceArray[y - 1, x - 1] && SourceArray[y, x] > SourceArray[y + 1, x + 1]))
+                        {
+                            OutputArray[y, x] = SourceArray[y, x];
+                        }
+                    } 
+                    else if ((G > 90 - 22.5 && G <= 90 + 22.5) || G > -90 - 22.5 && G <= -90 + 22.5) 
+                    {
+                        //for angles around 90 degrees (vertical) check north and south cells
+                        if ((SourceArray[y, x] > SourceArray[y - 1, x] && SourceArray[y, x] >= SourceArray[y + 1, x]) ||
+                            (SourceArray[y, x] >= SourceArray[y - 1, x] && SourceArray[y, x] > SourceArray[y + 1, x]))
+                        {
+                            OutputArray[y, x] = SourceArray[y, x];
+                        }
+                    } 
+                    else if ((G > 135 - 22.5 && G <= 135 + 22.5) || (G > -45 - 22.5 && G <= -45 + 22.5)) 
+                    {
+                        //for angles around 135 degrees (y increases as x increases) check north east and south west
+                        if ((SourceArray[y, x] > SourceArray[y - 1, x + 1] && SourceArray[y, x] >= SourceArray[y + 1, x - 1]) ||
+                            (SourceArray[y, x] >= SourceArray[y - 1, x + 1] && SourceArray[y, x] > SourceArray[y + 1, x - 1]))
+                            {
+                            OutputArray[y, x] = SourceArray[y, x];
+                            }
+                    } 
+
+                }
+            }
             return OutputArray;
         }
     }
